0f761406f66c0a3287a5d7fe382a18d4704ac36a
paint <111635886+watteni@users.noreply.github.com>
2024-06-24 22:00:46 +1000

    Create TrixtopsKillAll.lua

---
+++ TrixtopsKillAll.lua
@@ -0,0 +1,16 @@
+local remote = game:GetService("ReplicatedStorage"):WaitForChild("RCLStorage"):WaitForChild("Remotes"):WaitForChild("HitEvent")
+local players = game:GetService("Players"):GetPlayers()
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    for _, player in ipairs(players) do
+        local humanoid = player.Character and player.Character:FindFirstChild("Humanoid")
+        if humanoid then
+            local args = {
+                [1] = "scrub",
+                [2] = humanoid,
+                [3] = 10 
+            }
+            remote:FireServer(unpack(args))
+        end
+    end
+end)
