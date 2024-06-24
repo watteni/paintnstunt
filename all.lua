384b651228b4c363b07dbed97e333006395e4288
paint <111635886+watteni@users.noreply.github.com>
2024-06-24 21:21:27 +1000

    Update and rename all.txt to all.lua

---
+++ all.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/watteni/paintnstunt/main/44KillAll.lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/watteni/paintnstunt/main/RoRKillAll.lua'))()
