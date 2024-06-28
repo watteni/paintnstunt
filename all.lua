c62a6dfe6245ea4caed9289f8ce6999f13aab020
paint <111635886+watteni@users.noreply.github.com>
2024-06-28 21:15:49 +1000

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,6 +1,8 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/watteni/paintnstunt/main/TrixtopsKillAll.lua'))() -- Trixtops Kill All Script
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/watteni/paintnstunt/main/44KillAll.lua'))() -- Universal RoR Kill All Script
+loadstring(game:HttpGet('https://raw.githubusercontent.com/watteni/paintnstunt/main/TniKillAll.lua'))() -- Universal Tni Kill All Script
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/watteni/paintnstunt/main/44KillAll.lua'))() -- Universal 44th Kill All Script
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/watteni/paintnstunt/main/RoRKillAll.lua'))() -- Universal RoR Kill All Script
 
